322661716b627535bc92b9e5973e97047bc6e557
Max Harris <max.harris@snapdocs.com>
2025-08-19 09:29:56 -0400

    rename scripts, serialize projects and material requests on quotes

--- app/serializers/quote_serializer.rb
+++ app/serializers/quote_serializer.rb
@@ -4,10 +4,18 @@ class QuoteSerializer
   root_key :quote
   root_key_for_collection :quotes
 
-  attributes :id, :price, :lead_time_days, :status, :created_at, :updated_at
+  attributes :id, :supplier_id, :material_request_id, :price, :lead_time_days, :status, :created_at, :updated_at
 
-  attribute :material_request_id do |quote|
-    quote.material_request_id
+  attribute :project_id do |quote|
+    quote.material_request.project_id
+  end
+
+  attribute :project do |quote|
+    ProjectSerializer.new(quote.material_request.project).serialize
+  end
+
+  attribute :material_request do |quote|
+    MaterialRequestSerializer.new(quote.material_request).serialize
   end
 
   attribute :supplier do |quote|
